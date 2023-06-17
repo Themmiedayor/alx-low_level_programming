e10464100fc43e0e5dee75d8a52f7547bfb0f18d
Fapohunda Faith <fapohundafaith40@gmail.com>
2023-06-17 12:39:22 -0700

    Task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,7 +9,7 @@
 int main(void)
 {
 	int i = '0';
-	int j = '0';
+	int j = '1';
 
 	while (i <= '9')
 	{
@@ -31,7 +31,7 @@ int main(void)
 			}
 			j++;
 		}
-		j = '1';
+		j = '0';
 		i++;
 	}
 return (0);
