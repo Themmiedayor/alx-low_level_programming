fea44041aab0e2d30fdaa1aa909f9e5696e95398
Themmiedayor <fapohundafaith40@gmail.com>
2023-08-05 04:08:45 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,7 +10,7 @@
 size_t print_list(const list_t *h)
 {
 	size_t s = 0;
-	
+
 	while (h)
 	{
 		if (!h->str)
@@ -19,7 +19,6 @@ size_t print_list(const list_t *h)
 			printf("[%u] %s\n", h->len, h->str);
 		h = h->next;
 		s++;
-	}
-	
+	}	
 	return (s);
 }
